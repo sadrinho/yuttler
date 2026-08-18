9e08285484a86959859ed6b4c5de746d5ddc7932
Sadra <sadra.aliakbarpour@yale.edu>
2026-08-17 19:01:19 -0500

    Added "stops remaining" tracking. Worst feature I've ever added. Took 50 years. Let this be a cautionary tale.

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,74 @@
-import { useState, useEffect } from 'react'
-import { planTrip } from './tripPlanner'
-import Autocomplete from './Autocomplete'
-import Map from './Map'
+import { useState, useEffect } from "react";
+import { planTrip } from "./tripPlanner";
+import Autocomplete from "./Autocomplete";
+import Map from "./Map";
 
-function ResultsCard({ result, boardEtas }) {
-  if (!result) return <p>Enter a start and end location above</p>
-  if (!result.success) return <p>{result.message}</p>
-  if(result.walkOnly)
-  {
+function ResultsCard({ result, boardEtas, relevantEtas, trackedBus, onBoard, boarded, stopsRemaining }) {
+  if (!result) return <p>Enter a start and end location above</p>;
+  if (!result.success) return <p>{result.message}</p>;
+  if (result.walkOnly) {
     return (
       <div>
-        <p> No transit needed. Walk <strong>~{Math.round(result.distance)} meters</strong>  to your destination.</p>
+        <p>
+          {" "}
+          No transit needed. Walk{" "}
+          <strong>~{Math.round(result.distance)} meters</strong> to your
+          destination.
+        </p>
       </div>
-    )
+    );
   }
-
-  const relevantEtas = boardEtas
-      .filter(eta => eta.route === result.route.id) // we filter for routes only relevant to our trip result
-      .sort((a, b) => a.avg - b.avg) // sorts where a (eta obj 1)'s avg min comes before b's avg min
   
-
   return (
     <div>
-      <p>Walk to <strong>{result.boardStop.name}</strong></p>
-      <p>Board the <strong>{result.route.name}</strong></p>
-      {relevantEtas.length === 0
-        ? <p> No buses currently inbound for <strong>{result.boardStop.name}.</strong> </p> // TODO: later, suggest alternative stops or pull nearby stops
-        : relevantEtas.map(eta => 
-          <p key={eta.bus_id}> 
-            Bus <strong>{eta.bus_name}</strong> in <strong>{eta.avg}</strong> min.
-          </p>)
-      }
-      <p>Get off at <strong>{result.alightStop.name}</strong></p>
+      <p>
+        Walk to <strong>{result.boardStop.name}</strong>
+      </p>
+      <p>
+        Board the <strong>{result.route.name}</strong>
+      </p>
+      {relevantEtas.length === 0 ? (
+        <p>
+          {" "}
+          No buses currently inbound for{" "}
+          <strong>{result.boardStop.name}.</strong>{" "}
+        </p> // TODO: later, suggest alternative stops or pull nearby stops
+      ) : !boarded ?
+      (
+        <>
+        <p>{stopsRemaining} stops until you board bus{trackedBus?.name}</p>
+        {stopsRemaining && stopsRemaining <= 30 &&
+        (
+          <button onClick={onBoard}>I'm on board</button>
+        )
+        }
+        
+        {relevantEtas.map((eta) => ( // TODO: perhaps this is inappropriate for a presentational component?
+          <p key={eta.bus_id}>
+            Bus <strong>{eta.bus_name}</strong> in <strong>{eta.avg}</strong>{" "}
+            min.
+          </p>
+        ))}
+        </>
+      )
+      :
+      (
+        <>
+          <p>
+          Get off at <strong>{result.alightStop.name}</strong>
+          </p>
+          <p>
+            {stopsRemaining} stops remaining.
+          </p>
+        </>
+      )}
+      
     </div>
-  )
+  );
 }
 
 function App() {
-  const [stops, setStops] = useState([])
+  const [stops, setStops] = useState([]);
   /* useState() is a React function that creates a state variable (a piece of data that has a current value, and when that value is changed, react automatically re-renders it).
   we're saying "create a state variable, and its starting value is the empty array []"
   
@@ -49,102 +80,139 @@ function App() {
   without useState(), react wouldn't know to update the screen.
   by using setStops(data), we're saying "update the value of data AND refresh the screen"
   */
-  const [routes, setRoutes] = useState([])
-  const [buses, setBuses] = useState([])
-  const [boardEtas, setBoardEtas] = useState([])
+  const [routes, setRoutes] = useState([]);
+  const [buses, setBuses] = useState([]);
+  const [boardEtas, setBoardEtas] = useState([]);
 
-  const [tripResult, setTripResult] = useState(null)
+  const [tripResult, setTripResult] = useState(null);
+  const [startCoords, setStartCoords] = useState(null);
+  const [endCoords, setEndCoords] = useState(null);
+  const [boardedBusId, setBoardedBusId] = useState(null); // remains null until we board a bus
+  const [darkMode, setDarkMode] = useState(false);
 
-  const [startCoords, setStartCoords] = useState(null)
-  const [endCoords, setEndCoords] = useState(null)
+  // relevantEtas = sorted list of soonest arriving bus etas on our route
+  // when we have no tripResult, relevantEtas === []
+  
+  const relevantEtas = tripResult?.route // valid trip? has route?
+    ? boardEtas 
+      .filter((eta) => eta.route === tripResult.route.id) // we filter for routes only relevant to our trip result
+      .sort((a, b) => a.avg - b.avg) // sorts where a (eta obj 1)'s avg min comes before b's avg min
+    : []
 
-  const [darkMode, setDarkMode] = useState(false)
+  // our "tracked bus ID" should be the ID of the soonest arriving bus for our stop (relevantEtas[0].bus_id), unless we have boarded a bus, in which case it should remain pinned as that bus' ID.
+  // boardedBusId is null until we board a bus
+  const trackedBusId = boardedBusId ?? 
+  relevantEtas[0]?.bus_id
 
-  useEffect(() => { // this runs in response to something SPECIFIC, not every render
-    Promise.all([ // make sure both return something before moving on (both promises are fulfilled)
-      fetch(`${import.meta.env.VITE_PROXY_URL}/stops`).then(r => r.json()), // fetch http response object, then parse and return r.json()
-      // import.meta.env.VITE_PROXY_URL for referencing the correct back-end url according to VITE_PROXY_URL in our dotenv 
-      fetch(`${import.meta.env.VITE_PROXY_URL}/routes`).then(r => r.json()) // index 1
-    ]).then(([stopsData, routesData]) => { // ordered; stopsData = result[0], routesData = result[1]
-      setStops(stopsData)
-      setRoutes(routesData)
-    })
+  const trackedBus = buses.find(bus => bus.id === trackedBusId)
 
-  }, []) // [] is the dependency array indicating that specific thing, but sicne it's empty, it runs exactly once
+  
+
+  useEffect(() => {
+    // this runs in response to something SPECIFIC, not every render
+    Promise.all([
+      // make sure both return something before moving on (both promises are fulfilled)
+      fetch(`${import.meta.env.VITE_PROXY_URL}/stops`).then((r) => r.json()), // fetch http response object, then parse and return r.json()
+      // import.meta.env.VITE_PROXY_URL for referencing the correct back-end url according to VITE_PROXY_URL in our dotenv
+      fetch(`${import.meta.env.VITE_PROXY_URL}/routes`).then((r) => r.json()), // index 1
+    ]).then(([stopsData, routesData]) => {
+      // ordered; stopsData = result[0], routesData = result[1]
+      setStops(stopsData);
+      setRoutes(routesData);
+    });
+  }, []); // [] is the dependency array indicating that specific thing, but sicne it's empty, it runs exactly once
+
 
   useEffect(() => {
     function fetchBuses() {
       fetch(`${import.meta.env.VITE_PROXY_URL}/buses`)
-        .then(r => r.json())
-        .then(data => setBuses(data))
+        .then((r) => r.json())
+        .then((data) => setBuses(data));
     }
 
-    fetchBuses() 
+    fetchBuses();
 
-    const intervalId = setInterval(fetchBuses, 10000) //10000ms = 10s interval for now
+    const intervalId = setInterval(fetchBuses, 10000); //10000ms = 10s interval for now
 
-    return () => clearInterval(intervalId) // stops timer when the component unmounts
-
-  }, [])
+    return () => clearInterval(intervalId); // stops timer when the component unmounts
+  }, []);
 
   useEffect(() => {
-
-    if(!tripResult || !tripResult.success || !tripResult.boardStop)
-    {
-      setBoardEtas([])
-      return
+    if (!tripResult || !tripResult.success || !tripResult.boardStop) {
+      setBoardEtas([]);
+      return;
     }
-    
-    const stopId = tripResult.boardStop.id
+
+    const stopId = boarded? tripResult.alightStop : tripResult.boardStop.id;
 
     function fetchETA() {
-      fetch(`${import.meta.env.VITE_PROXY_URL}/eta/${stopId}`)
-        .then(r => r.json())
-        .then(data => { setBoardEtas(data?.etas?.[stopId]?.etas || []) })
-        // )
+      fetch(`${import.meta.env.VITE_PROXY_URL}/eta/${stopId}`) // fetches etas for our stopID
+        .then((r) => r.json())
+        .then((data) => {
+          setBoardEtas(data?.etas?.[stopId]?.etas || []); // note: 1) unsorted 2) returns etas for ALL routes containing boardStop
+        });
+      // )
     }
 
-    fetchETA() // immediate so it's not blank for the first 30s
+    fetchETA(); // immediate so it's not blank for the first 30s
 
-    const intervalId = setInterval(fetchETA, 30000) // 30s interval
+    const intervalId = setInterval(fetchETA, 30000); // 30s interval for updates
 
-    return () => clearInterval(intervalId) 
-
-  }, [tripResult]) // effect re-runs when tripResult updates
+    return () => clearInterval(intervalId);
+  }, [tripResult, boardedBusId]); // effect re-runs when tripResult updates or when our boardedBus updates (this usually should be whenever we select "im on board")
 
   function handleSearch() {
-
-    setTripResult(null)  // clear previous result first
+    setTripResult(null); // clear previous result first
+    setBoardedBusId(null) // reset boarding status
 
     if (!startCoords || !endCoords) {
-      setTripResult({ success: false, message: "Please select a start and end location" })
-      return
+      setTripResult({
+        success: false,
+        message: "Please select a start and end location",
+      });
+      return;
     }
 
     const result = planTrip(
-      startCoords.lat, startCoords.lon,
-      endCoords.lat, endCoords.lon,
+      startCoords.lat,
+      startCoords.lon,
+      endCoords.lat,
+      endCoords.lon,
       stops,
-      routes
-    )
+      routes,
+    );
 
-    setTripResult(result)
-
+    setTripResult(result);
   }
 
+  let stopsRemaining = null
+
+  if(tripResult?.success && tripResult.boardStop && trackedBus) // trackedBus guard incase no bus matches and therefore no etas
+  {
+    const targetStop = boarded? tripResult.alightStop : tripResult.boardStop // switches target calculation between "stops to get on" and "stops to get off"
+    const targetIndex = tripResult.route.stops.indexOf(targetStop.id)
+    const busIndex = tripResult.route.stops.indexOf(trackedBus.lastStop)
+    const routeLen = tripResult.route?.stops?.length
+    stopsRemaining = (targetStop === -1 || targetIndex === -1 || !routeLen)
+    ? null
+    : (targetIndex - busIndex + routeLen) % routeLen
+  }
 
   return (
     <div>
       <h1>Yale Shuttle</h1>
-      <p>Loaded {stops.length} stops, {routes.length} routes, {buses.length} buses</p>
+      <p>
+        Loaded {stops.length} stops, {routes.length} routes, {buses.length}{" "}
+        buses
+      </p>
       <div>
         <Autocomplete
           placeholder="Where are you starting from?"
-          onSelect={suggestion => setStartCoords(suggestion) }
+          onSelect={(suggestion) => setStartCoords(suggestion)}
         />
         <Autocomplete
           placeholder="Where are you going?"
-          onSelect={suggestion => setEndCoords(suggestion) }
+          onSelect={(suggestion) => setEndCoords(suggestion)}
         />
 
         <button onClick={handleSearch}>Find Route</button>
@@ -153,24 +221,26 @@ function App() {
           stops={stops}
           tripResult={tripResult}
           routes={routes}
-          darkMode = {darkMode}
+          darkMode={darkMode}
           buses={buses}
         />
 
-        <button onClick={() => setDarkMode(!darkMode)}> 
-          {darkMode ? 'Toggle Light Mode' : 'Toggle Dark Mode'}
+        <button onClick={() => setDarkMode(!darkMode)}>
+          {darkMode ? "Toggle Light Mode" : "Toggle Dark Mode"}
         </button>
-
       </div>
-      <ResultsCard
-        result={tripResult}
-        boardEtas={boardEtas}
+
+      <ResultsCard 
+        result={tripResult} 
+        boardEtas={boardEtas} 
+        relevantEtas={relevantEtas} 
+        trackedBus={trackedBus} 
+        onBoard={() => setBoardedBusId(trackedBusId)} // resultsCard tells react to call this when we trigger onBoard
+        boarded={boardedBusId !== null} // true if boarded, false otherwise
+        stopsRemaining= {stopsRemaining}
       />
-
-      
-
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
